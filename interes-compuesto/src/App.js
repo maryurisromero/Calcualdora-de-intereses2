0b514eae87408b34ce92aa17ce1f17a9bbd4d375
maryurisromero <mcrb02@gmail.com>
2022-01-27 09:38:40 -0500

    First commit

---
+++ interes-compuesto/src/App.js
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
+import Input from './components/Input'
+import Button from './components/Button'
+import Container from './components/Container'
+import Section from './components/Section'
+import Balance from './components/Balance'
+
+const compoundInterest = (deposit, contribution, years, rate) => {
+  let total = deposit
+  for (let i=0; i < years; i++ ){
+    total = ( total + contribution ) * (rate + 1)
+  } 
+  return Math.round(total)
+}
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'COP',
+  maximumFractionDigits: 2,
+  minimumFractionDigits: 2,
+})
+
+function App() {
+  const [ balance, setBalance ] = useState('')
+  const handleSubmit = ({ deposit, contribution, years, rate }) => {
+    const val = compoundInterest(Number(deposit), Number(contribution), Number(years), Number(rate))
+    setBalance(formatter.format(val));  
+  } 
+  return (
+    <Container>
+      <Section>
+      <Formik 
+        initialValues={{
+          deposit:'',
+          contribution:'',
+          years:'',
+          rate:'',
+        }}
+        onSubmit={handleSubmit}
+        validationSchema={Yup.object({
+          deposit: Yup.number().required('Obligatorio').typeError('Debe ser un número'),
+          contribution:Yup.number().required('Obligatorio').typeError('Debe ser un número'),
+          years:Yup.number().required('Obligatorio').typeError('Debe ser un número'),
+          rate:Yup
+            .number()
+            .required('Obligatorio')
+            .typeError('Debe ser un número')
+            .min(0, 'El valor minimo es de 0')
+            .max(1, 'El valor maximo debe ser 1'),
+        })}
+        >
+          <Form>
+            <Input name="deposit" label="Deposito inicial"/>
+            <Input name="contribution" label="Contribución mensual"/>
+            <Input name="years" label="Meses"/>
+            <Input name="rate" label="Interés mensual"/>
+            <Button type='submit'>Calcular</Button>
+          </Form>        
+      </Formik>
+      { balance !=='' ? <Balance>Balance final: {balance}</Balance> : null }
+      </Section>
+    </Container>
+  )
+}
+
+export default App
---
+++ interes-compuesto/src/components/Button.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const Button = styled.button`
+    background: palevioletred;
+    color: white;
+    font-size: 1em;
+    padding: .02em 1em;
+    border: 2px solid palevioletred;
+    border-radius: 4px;
+    cursor: pointer;
+`
+
+export default Button
